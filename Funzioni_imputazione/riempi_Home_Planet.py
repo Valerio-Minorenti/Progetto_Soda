b48111754448d5dbe3f441a19869ef047d37822d
davidevitale <dvitale2002@gmail.com>
2025-07-02 11:51:32 +0200

    Update riempi_Home_Planet.py

--- Funzioni_imputazione/riempi_Home_Planet.py
+++ Funzioni_imputazione/riempi_Home_Planet.py
@@ -1,24 +1,22 @@
 import pandas as pd
 
 def riempi_Home_Planet(df):
+    # Assicura che la colonna 'Group' esista
     if 'Group' not in df.columns:
         df['Group'] = df['PassengerId'].str.split('_').str[0].astype(int)
 
-    gruppi_multipli = df['Group'].value_counts()
-    gruppi_validi = gruppi_multipli[gruppi_multipli > 1].index
+    # Crea una tabella con il valore più frequente di HomePlanet per ogni Group (moda)
+    planet_group= df.dropna(subset=['HomePlanet']).groupby('Group')['HomePlanet'].agg(lambda x: x.mode().iloc[0] if not x.mode().empty else pd.NA)
 
-    homeplanet_gruppo = (
-        df[df['Group'].isin(gruppi_validi)]
-        .dropna(subset=['HomePlanet'])
-        .groupby('Group')['HomePlanet']
-        .agg(lambda x: x.mode()[0] if x.nunique() == 1 else None)
-        .dropna()
-        .to_dict()
-    )
+    # Trova gli indici dei passeggeri con HomePlanet mancante e Group presente in GHP_gb
+    riempi = df[(df['HomePlanet'].isna()) & (df['Group'].isin(planet_group.index))].index
 
-    df['HomePlanet'] = df.apply(
-        lambda row: homeplanet_gruppo.get(row['Group'], row['HomePlanet']) if pd.isna(row['HomePlanet']) else row['HomePlanet'],
-        axis=1
-    )
+    # Riempie i valori mancanti con la moda del gruppo
+    df.loc[riempi, 'HomePlanet'] = df.loc[riempi, 'Group'].map(planet_group)
+
+    # Conta i missing values dopo
+    missing_aft = df['HomePlanet'].isna().sum()
+
+    print('#HomePlanet missing values after:', missing_aft)
 
     return df
